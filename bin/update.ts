bb21315711790fef273635387d89b526c08ceb77
sizumita <github@smdr.io>
2024-05-08 17:43:36 +0900

    [chore] update

--- bin/update.ts
+++ bin/update.ts
@@ -10,15 +10,22 @@ const logger = new Logger("self update", {
 
 logger.info("Updating homebrew")
 await logger.complete("brew update")
+
 logger.info("Pulling dot")
 await logger.complete("git -C ~/dot pull")
+
 logger.info("Satisfy Brewfile")
 await logger.complete("brew bundle --file ($env.DOT_PATH | path join Brewfile)")
-await logger.info("Upgrade libraries")
+
+logger.info("Upgrade libraries")
 await logger.complete("brew upgrade")
-await logger.info("Update Brewfile")
+
+logger.info("Update Brewfile")
 await logger.complete("brew bundle dump -f --file ($env.DOT_PATH | path join Brewfile)")
-await logger.info("Cleanup")
+// update lock
+await logger.complete("brew bundle --file ($env.DOT_PATH | path join Brewfile)")
+
+logger.info("Cleanup")
 await logger.complete("brew cleanup")
 await import("./check.ts")
 logger.info("`self sync`コマンドを実行し変更を保存してください。")
