b1207ef242364928bfc3e6bf790b13efd12922a7
sizumita <github@smdr.io>
2024-10-10 05:14:02 +0900

    feat: add log

--- src/setup/main.ts
+++ src/setup/main.ts
@@ -6,12 +6,18 @@ import {setup_cargo_binaries, setup_rust} from "./cargo.ts";
 const dotPath = await $`echo "$HOME/dot"`.text().then(x => x.trim())
 
 
+console.log("   Setup rust")
 await setup_rust()
+console.log("   Setup symlink")
 await setup_cargo_binaries()
+console.log("   Setup 1password sock")
 await setup_1password_sock()
+console.log("   Setup xdg config")
 await setup_xdg_config()
+console.log("   Setup symlink")
 await setup_symlink()
 
 
 // Change repository remote url
+console.log("   Change remote url")
 await $`git -C ${dotPath} remote set-url origin git@github.com:sizumita/dot.git`
