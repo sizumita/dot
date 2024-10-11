737ce8f86866687568c3858fa37e27f424277b5f
sizumita <github@smdr.io>
2024-10-11 09:08:31 +0900

    Update aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ls=eza
-alias la=eza -a
-alias ll=eza -l
-alias self=bun run "${HOME}/dot/src/self.main.ts"
+alias la="eza -a"
+alias ll="eza -l"
+alias self='bun run "${HOME}/dot/src/self.main.ts"'
